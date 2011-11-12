0ebe0302f098a8e4c1d830644e7a4aa4b09ca241
Joshua Moerman <lakseru@gmail.com>
2011-11-12 14:51:18 +0100

    done

---
+++ text2png/Convert0rx.hpp
@@ -0,0 +1,76 @@
+//
+//  Tonemapper.hpp
+//  text2png
+//
+//  Created by Joshua Moerman on 11/12/11.
+//  Copyright 2011 Vadovas. All rights reserved.
+//
+
+#ifndef text2png_Tonemapper_hpp
+#define text2png_Tonemapper_hpp
+
+#include <iostream>
+#include "ImageFormatPNG.hpp"
+
+struct Convert0rx {
+	Convert0rx()
+	: width(0)
+	, height(0)
+	{
+		for(int i = 0; i < 256; ++i){
+			if('A' <= i && i <= 'Z'){
+				double norm = (i - 'A') / ('Z' - 'A' + 1.0);
+				colors[i] = color(norm * 0.8 + 0.2, 0.0, 0.0);
+			}
+			else if('a' <= i && i <= 'z'){
+				double norm = (i - 'a') / ('z' - 'a' + 1.0);
+				colors[i] = color(norm * 0.5 + 0.5, norm * 0.8 + 0.2, 0.0);
+			}
+			else if('0' <= i && i <= '9'){
+				double norm = (i - '0') / ('9' - '0' + 1.0);
+				colors[i] = color(0.0, norm * 0.8 + 0.2, norm * 0.5 + 0.5);
+			}
+			else if(isspace(i)){
+				colors[i] = color(0, 0, 0);
+			}
+			else if(iscntrl(i)){
+				colors[i] = color(255, i % 128, 255);
+			}
+			else if(ispunct(i)){
+				colors[i] = color(128 + i, 128 + i, 128 + i);
+			}
+			else {
+				colors[i] = color(0, i, 0);
+			}
+		}
+	}
+	
+	void analyze(std::istream & input){
+		std::string str;
+		while(std::getline(input, str)){
+			width = std::max(width, (uint32_t)str.size());
+			++height;
+		}
+	}
+	
+	void process(std::istream & input, ImageFormats::png::png_stream & image) const {
+		std::string str;
+		while(std::getline(input, str)){
+			int i = 0;
+			for (; i < str.size(); ++i) {
+				image << colors[str[i]];
+			}
+			for(; i < width; ++i) {
+				image << colors[' '];
+			}
+		}
+	}
+	
+	uint32_t width;
+	uint32_t height;
+	
+	typedef ImageFormats::png::png_stream::pixel color;
+	color colors[256];
+};
+
+#endif
--- text2png/main.cpp
+++ text2png/main.cpp
@@ -7,12 +7,30 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
 
-int main (int argc, const char * argv[])
-{
+#include "ImageFormatPNG.hpp"
+#include "Convert0rx.hpp"
 
-	// insert code here...
-	std::cout << "Hello, World!\n";
-    return 0;
+int main (int argc, const char * argv[]) try {
+	if (argc != 2) throw std::runtime_error("You must provide exactly 1 argument");
+	
+	Convert0rx c;
+	{	// we will stream EVERYTHING, that's why we need two passes.
+		std::ifstream file(argv[1]);
+		if (!file) throw std::runtime_error("File could not be opened");
+		c.analyze(file);
+	}
+	
+	{	// lazy way of rewinding the stream ;D.
+		std::ifstream file(argv[1]);
+		if (!file) throw std::runtime_error("File could not be opened");
+		ImageFormats::png::png_stream image(c.width, c.height, std::string(argv[1]) + ".png");
+		c.process(file, image);
+	}
+	
+} catch (std::exception & e){
+	std::cout << e.what() << std::endl;
 }
 
