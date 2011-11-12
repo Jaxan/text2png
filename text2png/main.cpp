f979363edf120a3ffdf329257ec515004ccf9067
Joshua Moerman <lakseru@gmail.com>
2011-11-12 13:49:24 +0100

    Initial Commit

---
+++ text2png/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  text2png
+//
+//  Created by Joshua Moerman on 11/12/11.
+//  Copyright 2011 Vadovas. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
